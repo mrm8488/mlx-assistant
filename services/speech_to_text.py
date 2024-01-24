5013ab96d643a48bed88eadb3fc514299a2bbc21
Manuel Romero <mrm8488@gmail.com>
2024-01-24 23:24:20 +0100

    Reformat file

--- services/speech_to_text.py
+++ services/speech_to_text.py
@@ -1,10 +1,9 @@
 import whisper
 
-
 class SpeechToText:
     def __init__(self, model_size="tiny"):
         self.model = whisper.load_model(model_size)
-
+        
     def transcribe(self, audio_file):
         result = self.model.transcribe(audio_file)
         return result["text"]
